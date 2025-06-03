47a2aa66a9963b9142e4675e5266bb7097359109
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-06-03 22:01:21 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -2,7 +2,8 @@ local aa = 0
 get('btn').onclick(function()
     aa = aa+1
     fetch({
-        url = 'https://example.com'
+        url = 'https://example.com',
+        method = 'GET'
     })
     get('amount').set_contents('clicked '..aa)
 end)
