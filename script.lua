71ad3cf59f80ae66f1b3b33704dd804f86d76396
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-06-03 22:11:55 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -3,7 +3,7 @@ local aa = 0;
 get('btn').on_click(function()
     aa = aa+1;
     fetch({
-        url = "https://fsh.plus",
+        url = "https://api.fsh.plus",
         method = "GET",
         headers = { ["Content-Type"] = "application/json" },
         body = ''
