7a9e3f2a83e9e701413899f736d436ae5fd38fc3
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-06-03 22:06:18 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,9 +1,12 @@
-local aa = 0
+local aa = 0;
+
 get('btn').onclick(function()
-    aa = aa+1
+    aa = aa+1;
     fetch({
-        url = 'https://example.com',
-        method = 'GET'
+        url = "https://example.com",
+        method = "GET",
+        headers = { ["Content-Type"] = "application/json" },
+        body = ''
     })
     get('amount').set_contents('clicked '..aa)
 end)
