b6b50e9c0d6c0b0048fa4877161363eeefd7d7cd
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-06-15 19:34:19 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,15 +1,8 @@
-print('start')
-get('search').on_click(function()
-    print('click')
-    local res = fetch({
-        url = "https://api.fsh.plus/wikipedia?page=" .. get('query').get_contents(),
-        method = "GET",
-        headers = { ["Content-Type"] = "application/json" },
-        body = ''
-    })
-    print('recived')
-    print(res)
-    get('title').set_contents(res.title)
-    get('text').set_contents(res.data)
-    print('changed')
-end)
+print(browser)
+print(global)
+global.cat = 'meow'
+print(global)
+print(location)
+print('Print')
+printw('Warn')
+printe('Error')
